532a94724934d20840d6e70bd6d3e3aaf202db3a
Gronomix <Gronomix2013@gmail.com>
2023-04-06 21:05:13 +0300

    1

--- gerl_boy.py
+++ gerl_boy.py
@@ -5,8 +5,9 @@ import random
 old_gerl = 0
 both_gerl = 0
 either_girl = 0
-
+boy = 0
 kids = ['boy', 'girl']
+
 for i in range(10000):
     younger = random.choice(kids)
     older = random.choice(kids)
@@ -17,8 +18,12 @@ for i in range(10000):
         both_gerl += 1
     if older == 'girl' or younger == ' girl':
         either_girl += 1
+    if older == 'boy':
+        boy += 1
+
 
 print(old_gerl)
 print(both_gerl)
 print(either_girl)
+print(boy)
 
