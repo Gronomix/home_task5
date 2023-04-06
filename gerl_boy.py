525bdcf08fc07e8edc9cbc7d9b86986f58c07f31
Gronomix <Gronomix2013@gmail.com>
2023-04-06 20:54:50 +0300

    1

---
+++ gerl_boy.py
@@ -0,0 +1,22 @@
+import itertools
+import numpy as np
+import random
+
+old_gerl = 0
+both_gerl = 0
+either_girl = 0
+
+kids = ['boy', 'girl']
+for i in range(10000):
+    younger = random.choice(kids)
+    older = random.choice(kids)
+
+    if older == 'girl':
+        old_gerl += 1
+    if older == 'girl' and younger == 'girl':
+        both_gerl += 1
+    if older == 'girl' or younger == ' girl':
+        either_girl += 1
+
+    print()
+
